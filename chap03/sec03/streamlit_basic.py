c7ebc80c546044e6a757a20f319d1ec56660a5fc
Hakkyun_OFFICE <hkromero41@gmail.com>
2025-10-02 12:44:37 +0900

    ㅇㅁㄴㅇㅁ

--- chap03/sec03/streamlit_basic.py
+++ chap03/sec03/streamlit_basic.py
@@ -4,14 +4,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
-# (0) 사이드바에서 api_key 입력하는 부분 
-with st.sidebar:
-    openai_api_key = os.getenv('OPENAI_API_KEY') 
-    # openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+api_key = st.secrets["openai"]["api_key"]
 
 st.title("37조 변학균 최지은 챕터8 점심은 뭐먹지 바로 고궁")
 
@@ -25,11 +18,11 @@ for msg in st.session_state.messages:
 
 # (3) 사용자 입력을 받아 대화 기록에 추가하고 AI 응답을 생성
 if prompt := st.chat_input():
-    if not openai_api_key:
+    if not api_key:
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
 
-    client = OpenAI(api_key=openai_api_key)
+    client = OpenAI(api_key=api_key)
     st.session_state.messages.append({"role": "user", "content": prompt}) 
     st.chat_message("user").write(prompt) 
     response = client.chat.completions.create(model="gpt-4o", messages=st.session_state.messages) 
